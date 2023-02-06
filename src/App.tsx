0740b634847418ceb61456cdc2f15ad423a99709
leanh1011 <leanh2194@gmail.com>
2023-02-06 11:35:18 +0700

    husky eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.scss'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
+  debugger
   debugger
   return (
     <div className="App">
